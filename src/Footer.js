d70ac29dbecfa296322eec73d1b534d37012c021
lindacolumbus <lindadcolumbus@gmail.com>
2021-11-23 17:10:49 -0500

    added modal display for error handling

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return(
         <footer>
-            <p>Created at <a href="https://junocollege.com/" target="_blank" rel="noreferrer">Juno College</a> by <a href="https://github.com/lindacolumbus?tab=repositories" target="_blank" rel="noreferrer">Linda Columbus</a></p>
+            <p>Created at <a href="https://junocollege.com/" target="_blank" rel="noreferrer">Juno College</a> by <a href="http://www.lindacolumbus.ca" target="_blank" rel="noreferrer">Linda Columbus</a></p>
             <p></p>
         </footer>
     )
--- src/MusicRecommendations.js
+++ src/MusicRecommendations.js
@@ -7,6 +7,9 @@ function MusicRecommendations(props) {
     // state to hold number of API results rendered to the page
     const [batch, setBatch] = useState(4);
 
+    // function for whether or not error message modal is visible or not
+    let [modalVisible, setModalVisible] = useState(false);
+
     // event handler, increase number of API results rendered to the page +4
     const moreResults = () => {
         setBatch(batch + 4)
@@ -15,7 +18,7 @@ function MusicRecommendations(props) {
     useEffect(() => {
         const proxiedUrl = 'https://tastedive.com/api/similar';
 
-        // Only return once if user passes a value and submits the form
+        // only return once if user passes a value and submits the form
         if (props.searchedMusician) {
 
             const url = new URL('https://proxy.hackeryou.com');
@@ -38,7 +41,7 @@ function MusicRecommendations(props) {
                     // make a copy of the array to not mutate original fetched data
                     const allData = [...results];
                     
-                    // Specific error handling since API always returns a successful call, even if it's an invalid parameter
+                    // specific error handling since API always returns a successful call, even if it's an invalid parameter
                     if (jsonResponse.Similar.Info[0].Type === 'music') {
                         setMusicReco(allData);
                         
@@ -47,9 +50,9 @@ function MusicRecommendations(props) {
                     }
                 })
                 .catch((error) => {
-                    // Modal error display
+                    // modal error display; if error occurs, let the modal be visible
                     if (error.message) {
-                        alert('caught the error')
+                        setModalVisible(true)
                     }
                 })
             }
@@ -59,15 +62,15 @@ function MusicRecommendations(props) {
     return (
         <>
             <section className="musicResults">
-                {/* Conditional operator, changing the on-screen message depending on whether or not the form has been submitted */}
-                {props.searchedMusician 
+                {/* conditional operator, changing the on-screen message depending on whether or not the form has been submitted */}
+                {musicReco.length >= 4
                 ? <p className="resultsHeading">Ok, so you like <span>{props.searchedMusician}</span>, but have you heard of...</p> 
                 : <p>You spin me right round, baby, right round...</p>}
 
                 {
                     musicReco.map(musician => {                            
                         return (
-                            // Pass retrieved API data as props to child component
+                            // pass retrieved API data as props to child component
                             <MusicResults
                                 key={musician.yID}
                                 name={musician.Name}
@@ -78,16 +81,23 @@ function MusicRecommendations(props) {
                         )}
                     )
                 } 
-
-                {/* Create component for modal? Pass toggle state as props??? */}
-
             </section>
             <div className="loadMore">
-                {/* Only show "load more results" button after first batch of four results are rendered */}
-                {props.searchedMusician
+                {/* only show "load more results" button after first batch of four results are rendered */}
+                {musicReco.length >= 4
                     ? <button onClick={moreResults}>Gimme, gimme more</button>
                     : null}
             </div>
+            {/* if error message occurs, display the modal */}
+            {modalVisible === true
+            ?
+                <div className="modal">
+                    <p>Can't find that musician! Want to try again? Honey, I'm still free, take a chance on me.</p>
+                    <button onClick={() => {setModalVisible(false)}}>Close</button>
+                </div>
+            : null
+            }
+
         </>
     )
 }
