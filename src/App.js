1bbba9e8ae01ece7f663992db680ef6015cf05cc
lindacolumbus <lindadcolumbus@gmail.com>
2022-01-11 13:21:35 -0500

    collapse header when searching added new class name for root  restructured components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,49 @@
 import './Styles.css';
-import Header from './components/Header';
-import DisplayResults from './components/DisplayResults';
+// import Header from './components/Header';
+// import DisplayResults from './components/DisplayResults';
 import Footer from './components/Footer';
+
+
+import { useState } from 'react';
+import UserSearchQuery from './components/UserSearchQuery';
+import MusicRecommendations from './components/MusicRecommendations';
+import SearchHistory from './components/SearchHistory';
+
+// function DisplayResults() {
+//     const [searchResult, setSearchResult] = useState('');
+
+//     const handleSubmit = (event, userInput) => {
+//         event.preventDefault();
+//         setSearchResult(userInput);
+//     }
 
 function App() {
 
+    const [searchResult, setSearchResult] = useState('');
+
+    const handleSubmit = (event, userInput) => {
+        event.preventDefault();
+        setSearchResult(userInput);
+    }
+
     return (
-        <div className="App">
+        <div className={searchResult ? 'searching' : ''}>
             <a href="#mainContent" class="skipLink">Skip to main content</a>
-            <Header />
+
+            <header>
+                <h1>Taste Maker</h1>
+            </header>
+
             <main class="wrapper" id="mainContent">
-                <DisplayResults />
+                <section className="results">
+                    {/* Pass down props to children components */}
+                    <UserSearchQuery
+                        searchValue={handleSubmit} />
+                    <MusicRecommendations
+                        searchedMusician={searchResult} />
+                    <SearchHistory
+                        searchedMusician={searchResult} />
+                </section>
             </main>
             <Footer />
         </div>
