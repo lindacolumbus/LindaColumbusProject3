3c82ac20a1ac7ee09b18071986f677d8ad27c155
Linda Columbus <lindadcolumbus@gmail.com>
2022-03-23 11:43:45 -0400

    changed proxy url

--- src/components/MusicRecommendations.js
+++ src/components/MusicRecommendations.js
@@ -19,7 +19,9 @@ function MusicRecommendations(props) {
 
     // only return once if user passes a value and submits the form
     if (props.searchedMusician) {
-      const url = new URL("https://proxy.hackeryou.com");
+      const url = new URL(
+        "https://thingproxy.freeboard.io/fetch/https://tastedive.com/api/similar"
+      );
       url.search = new URLSearchParams({
         reqUrl: proxiedUrl,
         "params[q]": props.searchedMusician,
